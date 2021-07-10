fe6ac4d4c4ac0ce81204ff39a67c12823aebcfa0
Henry <eplanas@edu.tecnocampus.cat>
2021-07-10 12:35:29 +0200

    1.0

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>AAAAAAAAA</Text>
+      <Text>Master</Text>
       <StatusBar style="auto" />
     </View>
   );
